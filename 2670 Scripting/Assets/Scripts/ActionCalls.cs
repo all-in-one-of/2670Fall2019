04bf42fb8261366b7f908aba5877eb2a7a4a0f52
Anthony Romrell <anthonyromrell@mac.com>
2019-09-18 14:22:58 -0600

    Using Unity Actions

---
+++ 2670 Scripting/Assets/Scripts/ActionCalls.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ActionCalls : MonoBehaviour
+{
+	public float speed = 1f, rotateSpeed = 10f, scaleSpeed = 0.02f;
+	private Vector3 location, rotations, scales;
+	private UnityAction transformAction;
+
+	private List<UnityAction> transActions;
+	
+	private void Start()
+	{
+		transformAction = OnMove;
+	}
+	private void OnMouseDown()
+	{
+		if (transformAction == OnMove)
+		{
+			transformAction = OnRotate;
+		}
+		
+		if (transformAction == OnRotate)
+		{
+			transformAction = OnScale;
+		} 
+		
+		if (transformAction == OnScale)
+		{
+			transformAction = OnMove;
+		}
+	}
+	void Update ()
+	{
+		transformAction();
+	}
+	public void OnMove()
+	{
+		location.x = speed * Time.deltaTime;
+		transform.Translate(location);
+	}
+	public void OnRotate()
+	{
+		rotations.y = rotateSpeed * Time.deltaTime;
+		transform.Rotate(rotations);
+	}
+	public void OnScale()
+	{
+		scales.Set(scaleSpeed,scaleSpeed,scaleSpeed);
+		transform.localScale += scales;
+	}
+}
